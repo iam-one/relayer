0bf566219fda9333a5bc28ce724c6d965c48311c
one <imone075@gmail.com>
2023-09-03 20:52:08 +0900

    init

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import serial
+import schedule
 from flask import Flask, request, jsonify
+from time import sleep
 
-# ser = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
+port = "/dev/ttyUSB0"
+port = "/dev/tty.usbmodem1101"
 ser = serial.Serial("/dev/tty.usbmodem1101", 9600, timeout=1)
+
+def checkHealth():
+    ser.write(str.encode("P\\n"))
+    while(True):
+        if ser.readable():
+            if str(ser.readline()).split("'")[1][:-2] == "OK\\r": break
+    print("Device is OK")
+
+schedule.every(5).seconds.do(checkHealth)
 
 app = Flask(__name__)
 
-@app.route('/device/<device>/state', methods = ['GET','PUT'])
-def update(device):
+@app.route('/device/<device>/state', methods = ['PUT'])
+def updateState(device):
     if request.method == 'PUT':
-        buffer = ""
         state = request.get_json()["state"]
 
         if state == "true":
@@ -23,23 +34,25 @@ def update(device):
 
         return 'Device %s Updated: %s'%(device, state)
 
-@app.route('/device/state')
+@app.route('/device/state', methods = ['GET'])
 def getState():
-    buffer = ""
-    data = {}
-    ser.write(str.encode("M\\n"))
-    while(True):
-        if ser.readable():
-            buffer += str(ser.readline()).split("'")[1][:-2]
-            if buffer != "" and str(ser.readline()).split("'")[1][:-2] == "OK\\r": break
+    if request.method == 'GET':
+        buffer = ""
+        data = {}
+        ser.write(str.encode("M\\n"))
+        while(True):
+            if ser.readable():
+                buffer += str(ser.readline()).split("'")[1][:-2]
+                if buffer != "" and str(ser.readline()).split("'")[1][:-2] == "OK\\r": break
 
-    for i in range(len(buffer)):
-        if buffer[i] == "T": data[i] = "true"
-        else: data[i] = "false"
+        for i in range(len(buffer)):
+            if buffer[i] == "T": data[i] = "true"
+            else: data[i] = "false"
 
-    return jsonify(data)
+        return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=9999)
-
-# Periodically send “P\n” and wait for “OK\n”.→ device health check.
+    while True:
+        schedule.run_pending()
+        sleep(1)
