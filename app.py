8939d786a3fce4717c79d3448a107b49c07407d6
one <imone075@gmail.com>
2024-02-27 00:22:53 +0900

    fix: clean code by PEP8

--- app.py
+++ app.py
@@ -3,23 +3,26 @@ import schedule
 from flask import Flask, request, jsonify
 from time import sleep
 
-port = "/dev/ttyUSB0"
 port = "/dev/tty.usbmodem1101"
-ser = serial.Serial("/dev/tty.usbmodem1101", 9600, timeout=1)
+ser = serial.Serial(port, 9600, timeout=1)
 
-def checkHealth():
+
+def check_health():
     ser.write(str.encode("P\\n"))
-    while(True):
+    while True:
         if ser.readable():
-            if str(ser.readline()).split("'")[1][:-2] == "OK\\r": break
+            if str(ser.readline()).split("'")[1][:-2] == "OK\\r":
+                break
     print("Device is OK")
 
-schedule.every(5).seconds.do(checkHealth)
+
+schedule.every(5).seconds.do(check_health)
 
 app = Flask(__name__)
 
-@app.route('/device/<device>/state', methods = ['PUT'])
-def updateState(device):
+
+@app.route('/device/<device>/state', methods=['PUT'])
+def update_state(device):
     if request.method == 'PUT':
         state = request.get_json()["state"]
 
@@ -28,28 +31,34 @@ def updateState(device):
         else:
             ser.write(str.encode("F " + str(device) + "\\n"))
 
-        while(True):
+        while True:
             if ser.readable():
-                if str(ser.readline()).split("'")[1][:-2] == "OK\\r": break
+                if str(ser.readline()).split("'")[1][:-2] == "OK\\r":
+                    break
 
-        return 'Device %s Updated: %s'%(device, state)
+        return 'Device %s Updated: %s' % (device, state)
 
-@app.route('/device/state', methods = ['GET'])
-def getState():
+
+@app.route('/device/state', methods=['GET'])
+def get_state():
     if request.method == 'GET':
         buffer = ""
         data = {}
         ser.write(str.encode("M\\n"))
-        while(True):
+        while True:
             if ser.readable():
                 buffer += str(ser.readline()).split("'")[1][:-2]
-                if buffer != "" and str(ser.readline()).split("'")[1][:-2] == "OK\\r": break
+                if buffer != "" and str(ser.readline()).split("'")[1][:-2] == "OK\\r":
+                    break
 
         for i in range(len(buffer)):
-            if buffer[i] == "T": data[i] = "true"
-            else: data[i] = "false"
+            if buffer[i] == "T":
+                data[i] = "true"
+            else:
+                data[i] = "false"
 
         return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=9999)
